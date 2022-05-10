f94567d100246e657e62392bc17a9282986bdb1a
Marat Remushev <mremushev@gmail.com>
2022-05-10 17:23:29 +0700

    Code refactor

--- flask-template/main.py
+++ flask-template/main.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('index.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('index.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
--- standart-template/scripts/index.js
+++ standart-template/scripts/index.js
@@ -1,13 +1,13 @@
 const navBtn = document.querySelector('nav').querySelectorAll('a');
-let activeBtn
+let activeBtn;
 
 navBtn.forEach(btn => {
 	if (btn.classList.contains('active')) {
-		activeBtn = btn
+		activeBtn = btn;
 	}
 	btn.addEventListener('click', () => {
-		activeBtn.classList.remove('active')
+		activeBtn.classList.remove('active');
 		btn.classList.add('active');
-		activeBtn = btn
-	})
+		activeBtn = btn;
+	});
 });
