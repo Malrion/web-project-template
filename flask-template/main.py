1a04a6186ef76a0c4891d62ab7512c033a5aac04
Marat Remushev <mremushev@gmail.com>
2022-10-12 15:58:23 +0700

    Fixed all basic css styles for body selection and removed unnecessary routes in flask

--- flask-template/main.py
+++ flask-template/main.py
@@ -6,25 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('index.html')
-
-@app.route('/pricing')
-def pricing():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('index.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('index.html')
-
-@app.route('/faq')
-def faq():
-    return render_template('index.html')
-
 if __name__ == '__main__':
     app.run()
